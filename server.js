4b793e3c322b9eda116b5bc8fe5975c6cdba1815
Michael Ehinger <mehinger@gmail.com>
2021-07-10 09:33:12 -0400

    third

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var item = require("./item.js");
 const { Console } = require("console");
 
 var sessionStore = new MongoStore({
-  url: process.env.MONGODB_FULL_URL || process.env.ORMONGO_RS_URL,
+  url: process.env.MONGODB_FULL_URL || process.env.ORMONGO_URL+'/mydb',
 });
 app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
